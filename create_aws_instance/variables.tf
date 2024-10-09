35837788fb5f51cbf22833c4ddff3bbf50299df4
Faizaan Wani <faizaanwani@Faizaans-MacBook-Pro.local>
2024-10-09 09:09:40 +0530

    Added AWS terraform

---
+++ create_aws_instance/variables.tf
@@ -0,0 +1,12 @@
+variable instance_type {
+  type        = string
+  default     = "t2.micro"
+  description = "description"
+}
+
+variable instance_tag_name {
+  type        = string
+  default     = "NewAppServerInstance"
+  description = "description"
+}
+
